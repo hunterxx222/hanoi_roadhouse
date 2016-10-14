bab3916307e0d8e467ffc20d005db345a9bf8633
Toan Ngo <xhunter9x@gmail.com>
2016-10-14 10:28:28 +0700

    removed ruby 2.3.0 folder in vendor

--- vendor/bundle/ruby/2.3.0/gems/jquery-tablesorter-1.22.7/lib/jquery-tablesorter/version.rb
+++
@@ -1,7 +0,0 @@
-module JqueryTablesorter
-  MAJOR = 1
-  MINOR = 22
-  TINY  = 7
-
-  VERSION = [MAJOR, MINOR, TINY].compact.join('.')
-end
--- vendor/bundle/ruby/2.3.0/gems/jquery-tablesorter-1.22.7/vendor/assets/javascripts/jquery-tablesorter.js
+++
@@ -1,3 +0,0 @@
-//= require jquery-tablesorter/extras/jquery.metadata
-//= require jquery-tablesorter/jquery.tablesorter
-//= require jquery-tablesorter/jquery.tablesorter.widgets
